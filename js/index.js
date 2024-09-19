33538fa7568ab309072cb5964ff3eb048c8dba06
abubakar308 <mdabubakarsiddique789@gmail.com>
2024-09-19 15:03:07 +0600

    Added homepage.html and other

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ document.getElementById('login-button').addEventListener('click',function(event)
    const pinNumber = document.getElementById('input-pin').value;
 
     if(phoneNumber === '5' && pinNumber === '1234'){
-        window.location.href = "https://abubakar308.github.io/login-page/homepage.html";
+        window.location.href = "pages/homepage.html";
     }
     else{
         alert('wrong number or pin');
