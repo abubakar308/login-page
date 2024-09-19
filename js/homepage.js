a5ce2f92da635a83f0f51ae8ff9c24c7ed5f9986
abubakar308 <mdabubakarsiddique789@gmail.com>
2024-09-19 17:52:32 +0600

    update information

--- js/homepage.js
+++ js/homepage.js
@@ -7,3 +7,38 @@ document.getElementById('cash-out-button').addEventListener('click',function(){
     document.getElementById('add-money').classList.add('hidden');
     document.getElementById('cash-out').classList.remove('hidden');
 })
+  let balance = document.getElementById('balance');
+  document.getElementById('add-click-button').addEventListener('click',function(event){
+    event.preventDefault();
+
+    let currentBalance = Number(balance.innerText);
+    const addPin = document.getElementById('add-pin').value;
+    
+    const addAmount = Number(document.getElementById('add-amount').value);
+    if(addPin === '1234'){
+      currentBalance += addAmount;
+      balance.innerText = currentBalance;
+     
+    }
+    else{
+        alert('wrong pin')
+    }
+  })
+
+  document.getElementById('out-click-button').addEventListener('click',function(event){
+    event.preventDefault();
+    let newBalance = Number(balance.innerText);
+    const outPin = document.getElementById('out-pin').value;
+    const demo = document.getElementById('demo')
+    const outAmount = Number(document.getElementById('out-amount').value);
+    if(outPin === '1234' && newBalance>=outAmount){
+      newBalance -= outAmount;
+      balance.innerText = newBalance;
+    }
+    else if(outPin !== '1234'){
+        alert('wrong pin')
+    }
+    else{
+        alert('insufficient balance please add balance')
+    }
+  });
--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ document.getElementById('login-button').addEventListener('click',function(event)
    const pinNumber = document.getElementById('input-pin').value;
 
     if(phoneNumber === '5' && pinNumber === '1234'){
-        window.location.href = "pages/homepage.html";
+        window.location.href = "./homepage.html";
     }
     else{
         alert('wrong number or pin');
